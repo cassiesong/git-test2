e8d67c168b1d53441314285571a47245456c84ba
songcaixia <cassiesong@163.com>
2022-07-26 19:09:32 +0800

    third commit idea

--- src/main/java/com/qau/git/GitTest/GitTest.java
+++ src/main/java/com/qau/git/GitTest/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("Hello git!!");
         System.out.println("Hello git!!");
-
+        System.out.println("Hello git!!");
     }
 }
