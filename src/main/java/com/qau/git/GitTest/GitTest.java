851431a23f366b6225dce19f170d0b9f25f299b8
songcaixia <cassiesong@163.com>
2022-07-26 19:43:40 +0800

    hot-fix  commit1 idea

--- src/main/java/com/qau/git/GitTest/GitTest.java
+++ src/main/java/com/qau/git/GitTest/GitTest.java
@@ -2,8 +2,9 @@ package com.qau.git.GitTest;
 
 public class GitTest {
     public static void main(String[] args){
-        System.out.println("Hello git!!");
-        System.out.println("Hello git!!");
-        System.out.println("Hello git!!");
+        System.out.println("Hello git 1!!");
+        System.out.println("Hello git 2!!");
+        System.out.println("Hello git!! 3");
+        System.out.println("Hello git!! 4");
     }
 }
