bf2b9c601819ce4d6db6f922952d460802a76dbc
songcaixia <cassiesong@163.com>
2022-07-26 19:08:36 +0800

    second commit idea

--- src/main/java/com/qau/git/GitTest/GitTest.java
+++ src/main/java/com/qau/git/GitTest/GitTest.java
@@ -3,5 +3,7 @@ package com.qau.git.GitTest;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("Hello git!!");
+        System.out.println("Hello git!!");
+
     }
 }
